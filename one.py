08c8ed0e4ae73e9ef56eb2b36a39c0bc3fddd3ec
sseen <ssnfee@gmail.com>
2017-09-07 16:23:07 +0800

    add

---
+++ one.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+"""This script parse the content of a xml file"""
+
+import telegram
+BOOT = telegram.Bot(token='286280920:AAETT06pvHpR8JQjH47Znd5pn_5VbUqp16Y')
+print BOOT.get_me()
